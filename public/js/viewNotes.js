d0ae6514af0cd49860f6e3942ca6d435b9df6389
TiloRC <tiloreneau@gmail.com>
2021-07-28 18:54:54 +0000

    very descriptive message

--- public/js/viewNotes.js
+++ public/js/viewNotes.js
@@ -27,13 +27,24 @@ const renderDataAsHtml = (data) => {
   for (const noteItem in data) {
     const note = data[noteItem];
     // For each note create an HTML card
-    cards += createCard(note)
+    if (window.location.pathname === "/viewArchievedNotes.html") {
+        if (note.archived === true) {
+            cards += createArchivedCard(note, noteItem)
+        }
+        
+    }
+    if (window.location.pathname === "/viewNotes.html") {
+        if (!note.archived === true) {
+            cards += createCard(note, noteItem)
+        }
+    } 
+
   };
   // Inject our string of HTML into our viewNotes.html page
   document.querySelector('#app').innerHTML = cards;
 };
 
-const createCard = (note) => {
+const createCard = (note, noteId) => {
   return `
     <div class="column is-one-quarter">
       <div class="card">
@@ -43,7 +54,95 @@ const createCard = (note) => {
         <div class="card-content">
           <div class="content">${note.text}</div>
         </div>
+        <footer class="card-footer">
+            <a href="#" class="card-footer-item" onclick="editNote('${noteId}')">
+                Edit
+            </a>
+            <a href="#" class="card-footer-item" onclick="deleteNote('${noteId}')">
+                Delete
+            </a>
+            <a href="#" class="card-footer-item" onclick="archiveNote('${noteId}')">
+                Archive
+            </a>
+        </footer>
       </div>
     </div>
   `;
+}
+
+
+const createArchivedCard = (note, noteId) => {
+  return `
+    <div class="column is-one-quarter">
+      <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">${note.title}</p>
+        </header>
+        <div class="card-content">
+          <div class="content">${note.text}</div>
+        </div>
+        <footer class="card-footer">
+            <a href="#" class="card-footer-item" onclick="unarchiveNote('${noteId}')">
+                Unarchive
+            </a>
+        </footer>
+      </div>
+    </div>
+  `;
+}
+
+
+
+
+const deleteNote = (noteId) => {
+    firebase.database().ref(`users/${googleUserId}/${noteId}`).remove();
+}
+
+
+const editNote = (noteId) => {
+    const editNoteModal = document.querySelector("#editNoteModal");
+    const notesRef = firebase.database().ref(`users/${googleUserId}`);
+    notesRef.on("value", (snapshot) => {
+        const data = snapshot.val();
+        const note = data[noteId];
+        document.querySelector("#editTitleInput").value = note.title;
+        document.querySelector("#editTextInput").value = note.text;
+        document.querySelector("#editNoteId").value = noteId;
+    })
+    editNoteModal.classList.toggle("is-active");
+}
+
+const archiveNote = (noteId) => {
+    console.log("archiveNote called")
+    const noteEdits = {
+        archived: true
+    }
+    firebase.database().ref(`users/${googleUserId}/${noteId}`).update(noteEdits)
+
+}
+
+const unarchiveNote = (noteId) => {
+    console.log("archiveNote called")
+    const noteEdits = {
+        archived: false
+    }
+    firebase.database().ref(`users/${googleUserId}/${noteId}`).update(noteEdits)
+
+}
+
+const closeEditModal = () => {
+    const editNoteModal = document.querySelector('#editNoteModal');
+    editNoteModal.classList.toggle('is-active');
+}
+
+const saveEditedNote = () => {
+    console.log("SaveEditedNote called");
+    const noteId = document.querySelector("#editNoteId").value;
+    const noteTitle = document.querySelector("#editTitleInput").value;
+    const noteText = document.querySelector("#editTextInput").value;
+    const noteEdits = {
+        title: noteTitle,
+        text: noteText
+    }
+    firebase.database().ref(`users/${googleUserId}/${noteId}`).update(noteEdits);
 }
